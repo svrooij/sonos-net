9534f889f2ba9a356936c0da632a3773599c1be9
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2022-10-26 14:12:32 +0200

    code: Sample code

---
+++ src/Sonos.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Sonos.Base;
+
+Console.WriteLine("Hello, World!");
+
+var sonos = new SonosDevice(new Uri("http://192.168.x.x:1400/"), null, null);
+// await sonos.AVTransportService.Play(new Sonos.Base.Services.AVTransportService.PlayRequest(){Speed = "1"});
+
+// var bass = await sonos.RenderingControlService.GetBass();
+// var info = await sonos.DevicePropertiesService.GetZoneInfo();
+var led = await sonos.DevicePropertiesService.GetLEDState();
+Console.WriteLine();
