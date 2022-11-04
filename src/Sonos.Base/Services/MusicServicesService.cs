791733e60e3a54c7841ae58ecfce293302401a5d
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2022-11-04 12:18:20 +0100

    chore: Added license header to each file

--- src/Sonos.Base/Services/MusicServicesService.cs
+++ src/Sonos.Base/Services/MusicServicesService.cs
@@ -1,23 +1,40 @@
-﻿using Sonos.Base.Soap;
+﻿/*
+ * Sonos-net
+ *
+ * Repository https://github.com/svrooij/sonos-net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-namespace Sonos.Base.Services
+using Sonos.Base.Soap;
+
+namespace Sonos.Base.Services;
+
+public partial class MusicServicesService
 {
-    public partial class MusicServicesService
+    public partial class ListAvailableServicesResponse
     {
-        public partial class ListAvailableServicesResponse
-        {
-            private MusicServiceCollection _musicServiceCollection;
+        private MusicServiceCollection _musicServiceCollection;
 
-            public MusicService[] MusicServices
+        public MusicService[] MusicServices
+        {
+            get
             {
-                get
+                if (_musicServiceCollection is null)
                 {
-                    if (_musicServiceCollection is null)
-                    {
-                        _musicServiceCollection = SoapFactory.ParseEmbeddedXml<MusicServiceCollection>(this.AvailableServiceDescriptorList);
-                    }
-                    return _musicServiceCollection.Services;
+                    _musicServiceCollection = SoapFactory.ParseEmbeddedXml<MusicServiceCollection>(this.AvailableServiceDescriptorList);
                 }
+                return _musicServiceCollection.Services;
             }
         }
     }
