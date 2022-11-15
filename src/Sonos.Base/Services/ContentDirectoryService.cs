fd35fb88fdf509c7daf8ff016970886ff77e6ada
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2022-11-15 23:39:25 +0100

    ci: Adding comments to prevent warnings

--- src/Sonos.Base/Services/ContentDirectoryService.cs
+++ src/Sonos.Base/Services/ContentDirectoryService.cs
@@ -27,6 +27,11 @@ public partial class ContentDirectoryService
     /// Browse with defaults
     /// </summary>
     /// <param name="ObjectID">Object ID for browsing</param>
+    /// <param name="BrowseFlag"></param>
+    /// <param name="Filter"></param>
+    /// <param name="StartingIndex"></param>
+    /// <param name="Count"></param>
+    /// <param name="SortCriteria"></param>
     /// <param name="cancellationToken">CancellationToken</param>
     /// <remarks>(1) If the title contains an apostrophe the returned uri will contain a `&apos;`. (2) Some libraries support a BrowseAndParse, so you don't have to parse the xml.</remarks>
     /// <returns>BrowseResponse</returns>
@@ -43,9 +48,36 @@ public partial class ContentDirectoryService
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Browse the current queue
+    /// </summary>
+    /// <param name="count">Number of items to load</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task<BrowseResponse> CurrentQueue(int count = 100, CancellationToken cancellationToken = default) => Browse("Q:0", Count: count, cancellationToken: cancellationToken);
+    
+    /// <summary>
+    /// Browse the favorite radio shows
+    /// </summary>
+    /// <param name="count"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task<BrowseResponse> FavoriteRadioShows(int count = 100, CancellationToken cancellationToken = default) => Browse("R:0/1", Count: count, cancellationToken: cancellationToken);
+
+    /// <summary>
+    /// Browse the favorite radio stations
+    /// </summary>
+    /// <param name="count">Number of items to load</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task<BrowseResponse> FavoriteRadioStations(int count = 100, CancellationToken cancellationToken = default) => Browse("R:0/0", Count: count, cancellationToken: cancellationToken);
+
+    /// <summary>
+    /// Browse favorites
+    /// </summary>
+    /// <param name="count">Number of items to load</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task<BrowseResponse> Favorites(int count = 100, CancellationToken cancellationToken = default) => Browse("FV:2", Count: count, cancellationToken: cancellationToken);
 
     public partial class BrowseResponse
